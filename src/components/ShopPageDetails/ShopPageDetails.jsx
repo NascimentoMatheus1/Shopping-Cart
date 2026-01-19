a0541410651a8b6cd89dae382ca7abed0be73d3f
Matheus Nascimento <matheus_nascimento13@hotmail.com>
2026-01-18 21:44:53 -0300

    Feat: Add shop page details for the dynamic params

---
+++ src/components/ShopPageDetails/ShopPageDetails.jsx
@@ -0,0 +1,31 @@
+import Product from '../Product/Product';
+import { ClipLoader } from 'react-spinners';
+import { useOutletContext, useParams } from 'react-router';
+import styles from './ShopPageDetails.module.css';
+
+function ShopPageDetails() {
+    const { data, loading, error, getProductByCategory } = useOutletContext();
+    let { category } = useParams();
+    const productByCategory = getProductByCategory(category);
+
+    if (loading)
+        return (
+            <div className={styles.shopPage}>
+                <ClipLoader className={styles.loadingIcon} />
+            </div>
+        );
+
+    if (error) return <div className={styles.shopPage}>{error}</div>;
+
+    return (
+        <div className={styles.shopPage}>
+            <div className={styles.productsContainer}>
+                {productByCategory.map((item) => (
+                    <Product {...item} />
+                ))}
+            </div>
+        </div>
+    );
+}
+
+export default ShopPageDetails;
